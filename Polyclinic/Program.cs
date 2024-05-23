64af44f9f3af85aac362517f72f0c36c06e903f9
AleksandrGHub <bulanov.shura@gmail.com>
2024-05-23 22:04:15 +0300

    Добавьте файлы проекта.

---
+++ Polyclinic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Polyclinic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityPatientQueue;
+            int timeReceptionPatient = 10;
+            int quantityMinutesInHour = 60;
+            int hoursWait;
+            int minutesWait;
+            int timeWait;
+
+            Console.Write("Введите кол-во старушек в очереди: ");
+            quantityPatientQueue = Convert.ToInt32(Console.ReadLine());
+
+            timeWait = quantityPatientQueue * timeReceptionPatient;
+            hoursWait = timeWait / quantityMinutesInHour;
+            minutesWait = timeWait % quantityMinutesInHour;
+
+            Console.WriteLine($"Время ожидания: {hoursWait} ч, {minutesWait} мин.");
+        }
+    }
+}
